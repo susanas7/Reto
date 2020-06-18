3e8afb338db0d7a664206424c0cbe24a91e68d3a
Susana Santamaria <susana.s7@hotmail.com>
2020-06-18 00:27:05 -0500

    Estructura basica, pag ppal, login, registro

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>MercaTodo</title>
+    <style>
+    body
+    {
+
+    }
+    h1
+    {
+      font-size:50px;
+      font-family:Helvetica;
+
+    }
+    </style>
+</head>
+<body>
+   <h1><center>Bienvenidos a MercaTodo<center></h1>
+   <center><a href='/home'><input type="button" value="Ingresar"></a></center>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
+//Pagina principal
+Route::view('/', 'index');
 
+//Pagina por defecto, login y registro
+Auth::routes();
 
-//('/nombre de la ruta q queremos crear', 'nombre del archivo a llamar(home.blade.php)
-//')->name es buena practica
-Route::view('/', 'home')->name('home');
-Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
-Route::view('/contact', 'contact')->name('contact');
+Route::get('/home', 'HomeController@index')->name('home');
 
-});
 ?>
